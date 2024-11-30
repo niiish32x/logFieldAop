bf8912d44d76b037c2eaa72d1277ef11d7e4ba01
nishenghao <1159417019@qq.com>
2024-11-30 20:41:07 +0800

    统一 repo 方法规则

--- src/main/java/org/niiish32x/logfieldaop/dao/customer/persistence/repo/impl/CustomerRepoImpl.java
+++ src/main/java/org/niiish32x/logfieldaop/dao/customer/persistence/repo/impl/CustomerRepoImpl.java
@@ -1,8 +1,9 @@
 package org.niiish32x.logfieldaop.dao.customer.persistence.repo.impl;
 
+import org.niiish32x.logfieldaop.dao.customer.CustomerDO;
 import org.niiish32x.logfieldaop.dao.customer.persistence.dao.CustomerDAO;
 import org.niiish32x.logfieldaop.dao.customer.persistence.converter.CustomerConverter;
-import org.niiish32x.logfieldaop.dao.customer.persistence.CustomerDO;
+
 import org.niiish32x.logfieldaop.dao.customer.persistence.repo.CustomerRepo;
 import org.niiish32x.logfieldaop.domain.entity.CustomerEO;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Repository
+
 public class CustomerRepoImpl implements CustomerRepo {
 
     @Resource
@@ -19,9 +20,12 @@ public class CustomerRepoImpl implements CustomerRepo {
 
     CustomerConverter converter = CustomerConverter.INSTANCE;
 
+
     @Override
-    public  List<CustomerEO> findAllCustomers() {
+    public List<CustomerEO> findAllCustomers() {
+
         List<CustomerDO> customerDOS = customerDAO.lambdaQuery().list();
+
         return customerDOS.stream().map(converter::toEO).collect(Collectors.toList());
     }
 }
