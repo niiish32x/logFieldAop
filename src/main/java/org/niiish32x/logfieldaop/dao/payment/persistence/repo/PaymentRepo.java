ef3e12c443192e9e966f2455c1efc496b5e8c231
nishenghao <1159417019@qq.com>
2024-11-30 20:40:39 +0800

    save

--- src/main/java/org/niiish32x/logfieldaop/dao/payment/persistence/repo/PaymentRepo.java
+++ src/main/java/org/niiish32x/logfieldaop/dao/payment/persistence/repo/PaymentRepo.java
@@ -1,9 +1,24 @@
 package org.niiish32x.logfieldaop.dao.payment.persistence.repo;
 
+import org.niiish32x.logfieldaop.common.Result;
 import org.niiish32x.logfieldaop.domain.entity.PaymentEO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface PaymentRepo {
-    List<PaymentEO> findAll();
+    List<PaymentEO> find();
+
+    PaymentEO find(long id);
+
+
+    /**
+     * 获取订单金额
+     * @param id
+     * @return
+     */
+    BigDecimal findAmount(long id);
+
+
+    boolean save(PaymentEO paymentEO);
  }
--- src/main/java/org/niiish32x/logfieldaop/dao/payment/persistence/repo/impl/PaymentRepoImpl.java
+++ src/main/java/org/niiish32x/logfieldaop/dao/payment/persistence/repo/impl/PaymentRepoImpl.java
@@ -1,5 +1,6 @@
 package org.niiish32x.logfieldaop.dao.payment.persistence.repo.impl;
 
+import org.niiish32x.logfieldaop.common.Result;
 import org.niiish32x.logfieldaop.dao.payment.persistence.dao.PaymentDAO;
 import org.niiish32x.logfieldaop.dao.payment.persistence.converter.PaymentConverter;
 import org.niiish32x.logfieldaop.dao.payment.PaymentDO;
@@ -8,6 +9,7 @@ import org.niiish32x.logfieldaop.domain.entity.PaymentEO;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,9 +24,72 @@ public class PaymentRepoImpl implements PaymentRepo {
 
 
     @Override
-    public List<PaymentEO> findAll() {
+    public List<PaymentEO> find() {
         List<PaymentDO> paymentDOS = paymentDAO.lambdaQuery().list();
 
         return paymentDOS.stream().map(converter::toEO).collect(Collectors.toList());
     }
+
+    @Override
+    public PaymentEO find(long id) {
+        PaymentDO paymentDO = paymentDAO.lambdaQuery().eq(PaymentDO::getId, id).one();
+        return converter.toEO(paymentDO);
+    }
+
+
+    @Override
+    public BigDecimal findAmount(long id) {
+        PaymentDO paymentDO = paymentDAO.lambdaQuery().eq(PaymentDO::getId, id).one();
+        return paymentDO.getAmount();
+    }
+
+    @Override
+    public boolean save(PaymentEO paymentEO) {
+
+        PaymentEO existPayment = find(paymentEO.getId());
+        if(existPayment != null) {
+            // 已存在 只更新
+            paymentEO.setPaymentDate(paymentEO.getPaymentDate());
+            paymentEO.setAmount(paymentEO.getAmount());
+            paymentEO.setPaymentMethod(paymentEO.getPaymentMethod());
+            paymentEO.setCustomerId(paymentEO.getCustomerId());
+            paymentEO.setPaymentStatus(paymentEO.getPaymentStatus());
+        }
+
+        PaymentDO saveDO = converter.toDO(paymentEO);
+        return paymentDAO.save(saveDO);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
