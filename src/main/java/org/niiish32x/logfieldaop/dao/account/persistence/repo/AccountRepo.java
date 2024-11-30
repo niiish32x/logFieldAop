b6281a5a63088e565d0d8b93180179d8d52af7db
nishenghao <1159417019@qq.com>
2024-11-30 21:20:02 +0800

    select for update

--- src/main/java/org/niiish32x/logfieldaop/dao/account/persistence/repo/AccountRepo.java
+++ src/main/java/org/niiish32x/logfieldaop/dao/account/persistence/repo/AccountRepo.java
@@ -3,9 +3,29 @@ package org.niiish32x.logfieldaop.dao.account.persistence.repo;
 
 import org.niiish32x.logfieldaop.domain.entity.AccountEO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface AccountRepo {
 
-    List<AccountEO> findAll();
+    List<AccountEO> find();
+
+    AccountEO find(Long id);
+
+    AccountEO findWithLock(Long id);
+
+    /**
+     * 获取账户余额
+     * @param id
+     * @return
+     */
+    BigDecimal getBalance(int id);
+
+    /**
+     * 扣除 账户余额
+     * @param id
+     * @param amount
+     * @return
+     */
+    boolean updateWithLock(long id, BigDecimal amount);
 }
--- src/main/java/org/niiish32x/logfieldaop/dao/account/persistence/repo/impl/AccountRepoImpl.java
+++ src/main/java/org/niiish32x/logfieldaop/dao/account/persistence/repo/impl/AccountRepoImpl.java
@@ -1,13 +1,19 @@
 package org.niiish32x.logfieldaop.dao.account.persistence.repo.impl;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.niiish32x.logfieldaop.dao.account.AccountDO;
 import org.niiish32x.logfieldaop.dao.account.persistence.converter.AccountConverter;
 import org.niiish32x.logfieldaop.dao.account.persistence.dao.AccountDAO;
 import org.niiish32x.logfieldaop.dao.account.persistence.repo.AccountRepo;
 import org.niiish32x.logfieldaop.domain.entity.AccountEO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,13 +22,90 @@ public class AccountRepoImpl implements AccountRepo {
 
     AccountConverter converter = AccountConverter.INSTANCE;
 
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate;
 
     @Resource
     AccountDAO accountDAO;
 
     @Override
-    public List<AccountEO> findAll() {
+    public List<AccountEO> find() {
         List<AccountDO> accountDOS = accountDAO.lambdaQuery().list();
         return accountDOS.stream().map(converter::toEO).collect(Collectors.toList());
     }
+
+    @Override
+    public AccountEO find(Long id) {
+        AccountDO accountDO = accountDAO.lambdaQuery().eq(AccountDO::getId, id).one();
+        return converter.toEO(accountDO);
+    }
+
+    @Override
+    public AccountEO findWithLock(Long id) {
+        AccountDO accountDO = accountDAO.lambdaQuery().eq(AccountDO::getId, id).last("FOR UPDATE").one();
+        return converter.toEO(accountDO);
+    }
+
+    @Override
+    public BigDecimal getBalance(int id) {
+        AccountDO accountDO = accountDAO.lambdaQuery().eq(AccountDO::getId, id).one();
+        return accountDO.getBalance();
+    }
+
+
+    @Override
+    public boolean updateWithLock(long id, BigDecimal amount) {
+
+        Wrapper wrapper = Wrappers.<AccountDO>lambdaUpdate()
+                .setSql("balance - " + amount)
+                .last("FOR UPDATE");
+
+        AccountEO accountEO = findWithLock(id);
+        AccountDO accountDO = converter.toDO(accountEO);
+        boolean saveRes = accountDAO.saveOrUpdate(accountDO, wrapper);
+
+        return saveRes;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
