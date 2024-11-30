d3c32d3a6ad26b85b0407b6bc2ece50b845f7f10
nishenghao <1159417019@qq.com>
2024-11-30 20:49:42 +0800

    Result 返回结果 接口

--- src/main/java/org/niiish32x/logfieldaop/common/Result.java
+++ src/main/java/org/niiish32x/logfieldaop/common/Result.java
@@ -1,6 +1,7 @@
 package org.niiish32x.logfieldaop.common;
 
 
+import com.alibaba.fastjson.JSON;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +9,11 @@ import java.io.Serializable;
 
 /**
  * 接口结果
+ *
+ * @param <T> data的类型参数
+ * @author huangjue
  */
 @Data
-@Builder
 public class Result<T> implements Serializable {
 
     private static final long serialVersionUID = -5202065562748397103L;
@@ -53,5 +56,88 @@ public class Result<T> implements Serializable {
         return code == SUCCESS_CODE;
     }
 
+    public String toJSON() {
+        return JSON.toJSONString(this);
+    }
+
+    public static Result<String> success() {
+        return success("OK");
+    }
+
+    public static <T> Result<T> success(T t) {
+        return Result.newBuilder()
+                .code(SUCCESS_CODE)
+                .message(SUCCESS)
+                .data(t)
+                .build();
+    }
+
+    public static <T> Result<T> error(Result r) {
+        return error(r.getCode(), r.getMessage());
+    }
+
+    public static <T> Result<T> error(int code, String message) {
+        return Result.newBuilder()
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+
+
+    public static <T> Result<T> error(int code, String msgPattern, Object... args) {
+        return error(code, String.format(msgPattern, args));
+    }
+
+    public static <T> Result<T> error(String msgPattern, Object... args) {
+        return error(String.format(msgPattern, args));
+    }
+
+    private Result(Builder<T> builder) {
+        setCode(builder.code);
+        setData(builder.data);
+        setMessage(builder.message);
+        setMsg(builder.message);
+        setDebug(builder.debug);
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static final class Builder<T> {
+        private int code;
+        private T data;
+        private String message;
+        private String debug;
+
+        private Builder() {
+        }
+
+        public Builder code(int code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder debug(String debug) {
+            this.debug = debug;
+            return this;
+        }
+
+        public Result build() {
+            return new Result(this);
+        }
+    }
 }
+
 
